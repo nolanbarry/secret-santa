82a4048dd99170b3a933e865b473cb6ddb2c881b
Nolan Barry <nolan.barry@live.com>
2022-09-27 13:43:46 -0600

    Updated CDK to deploy API gateway & DynamoDB tables

--- cdk/lib/lambdas.ts
+++ cdk/lib/lambdas.ts
@@ -2,8 +2,41 @@ import { Duration } from "aws-cdk-lib"
 import { ManagedPolicy, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam"
 import { Code, Function, Runtime, LayerVersion } from "aws-cdk-lib/aws-lambda"
 import { SecretSantaStack } from "./secret-santa-stack"
+import { Tables } from "./tables"
+
+/* When defining a lambda, add its name (in kebab case, name should match exactly the
+ * filename containing its handler) to the `Lambdas` type below. `configurationProps`
+ * is typed such that it will automatically enforce that a configuration exists for
+ * every name defined in `Lambdas`. */
+
+/** 
+ * This object is used to define typing for all objects containing information about
+ * each lambda. For example, the `createLambdas` object returns a (typed) object with
+ * each key named the same as a key (aka `lambda reference`) in this object and mapped 
+ * to a `Function` object.
+ */
+export type Lambdas = {
+  submitOtp: Function
+  login: Function,
+  getUser: Function,
+  createGame: Function,
+  joinGame: Function,
+  getGame: Function,
+  startGame: Function,
+  endGame: Function,
+  getPlayers: Function,
+  getPlayer: Function
+}
+
+/** 
+ * A collection of strings to index into objects that have some attribute for every
+ * lambda.
+ */
+export type LambdaReference = keyof Lambdas
 
 type LambdaConfigurationProps = {
+  /** The name of the lambda. Must be formatted-in-kebab-case. */
+  name: string,
   /** The api route to attach this lambda to, i.e. `/api/route` */
   apiRoute: string,
   /** The configured memory (in megabytes) for the lambda. Defaults to 128. */
@@ -14,6 +47,54 @@ type LambdaConfigurationProps = {
   description?: string
 }
 
+/** The contents of this object are used to configure each Lambda when deploying. See the 
+ * `LambdaConfigurationProps` type for more options available for configuration. The 
+ * configured type of this variable will automatically update to require a configuration
+ * for every lambda named in the `Lambdas` type.
+ */
+const configurationProps: { [Property in LambdaReference]: LambdaConfigurationProps } = {
+  submitOtp: {
+    name: 'submit-otp',
+    apiRoute: '/user/submitotp',
+  },
+  login: {
+    name: 'login',
+    apiRoute: '/user/login'
+  },
+  getUser: {
+    name: 'get-user',
+    apiRoute: '/user'
+  },
+  createGame: {
+    name: 'create-game',
+    apiRoute: '/game/create'
+  },
+  joinGame: {
+    name: 'join-game',
+    apiRoute: '/game/join'
+  },
+  getGame: {
+    name: 'get-game',
+    apiRoute: '/game'
+  },
+  startGame: {
+    name: 'start-game',
+    apiRoute: '/game/start'
+  },
+  endGame: {
+    name: 'end-game',
+    apiRoute: '/game/end'
+  },
+  getPlayers: {
+    name: 'get-players',
+    apiRoute: '/user/players'
+  },
+  getPlayer: {
+    name: 'get-player',
+    apiRoute: '/user/player'
+  }
+}
+
 class LambdaConfiguration {
   public name: string
   public cdkID: string
@@ -22,13 +103,15 @@ class LambdaConfiguration {
   public memory: number
   public timeout: Duration
   public description: string
+  public handler: string
 
-  constructor(name: string, props: LambdaConfigurationProps) {
-    const tokenizedName = name.split('-').map(([first, ...after]) => first.toUpperCase() + after.join(""));
-    this.name = name;
+  constructor(props: LambdaConfigurationProps) {
+    const tokenizedName = props.name.split('-').map(([first, ...after]) => first.toUpperCase() + after.join(""));
+    this.name = `secret-santa-${props.name}`;
     this.cdkID = tokenizedName.join("") + 'Lambda';
     this.friendlyName = tokenizedName.join(" ");
-
+    
+    this.handler = `lambda/${props.name}.default`
     this.apiRoute = props.apiRoute;
     this.memory = props.memory ?? 128;
     this.timeout = props.timeout ?? Duration.seconds(20);
@@ -36,109 +119,30 @@ class LambdaConfiguration {
   }
 }
 
-
-/* When defining a lambda, add its name (in kebab case, name should match exactly the
- * filename containing its handler) to the const below. `configurationProps`
- * is typed such that it will automatically enforce that a configuration exists for
- * every name defined in `Lambda`. */
-
-/** 
- * This object is used to define typing for all objects containing information about
- * each lambda. For example, the `createLambdas` object returns a (typed) object with
- * each key named the same as a key (aka `lambda reference`) in this object and mapped 
- * to a `Function` object.
- */
-const Lambda = {
-  submitOtp: 'submit-otp',
-  login: 'login',
-  getUser: 'get-user',
-  createGame: 'create-game',
-  joinGame: 'join-game',
-  getGame: 'get-game',
-  startGame: 'start-game',
-  endGame: 'end-game',
-  getPlayers: 'get-players',
-  getPlayer: 'get-player'
-} as const
-
-/** 
- * A type representing a union of all keys in the `Lambda` object. Other objects in this file
- * will use the same keys.
- */
-type LambdaReference = keyof typeof Lambda
-
-/** 
- * A type representing a union of all values in the `Lambda` object. e.g., the name of every
- * Lambda.
- */
-type LambdaName = typeof Lambda[keyof typeof Lambda];
-type DiscreteConfigurationProps = { [Property in LambdaName]: LambdaConfigurationProps }
-
-/** The contents of this object are use to configure each Lambda when deploying. See the 
- * `LambdaConfigurationProps` type for more options available for configuration. The 
- * `DiscreteConfigurationProps` type will automatically update to require a configuration
- * for every lambda named in the `Lambda` object.
- */
-const configurationProps: DiscreteConfigurationProps = {
-  [Lambda.submitOtp]: {
-    apiRoute: '/user/submitotp',
-  },
-  [Lambda.login]: {
-    apiRoute: '/user/login'
-  },
-  [Lambda.getUser]: {
-    apiRoute: '/user'
-  },
-  [Lambda.createGame]: {
-    apiRoute: '/game/create'
-  },
-  [Lambda.joinGame]: {
-    apiRoute: '/game/join'
-  },
-  [Lambda.getGame]: {
-    apiRoute: '/game'
-  },
-  [Lambda.startGame]: {
-    apiRoute: '/game/start'
-  },
-  [Lambda.endGame]: {
-    apiRoute: '/game/end'
-  },
-  [Lambda.getPlayers]: {
-    apiRoute: '/user/players'
-  },
-  [Lambda.getPlayer]: {
-    apiRoute: '/user/player'
-  }
-}
-
 /** 
  * Creates a map with the same keys as `Lambda`, except that instead of just the function name, the value
  * is the function configuration. 
- * */
-function buildConfigurationMap(): {[Property in keyof typeof Lambda]: LambdaConfiguration} {
-  const entries = Object.entries<LambdaName>(Lambda) as [LambdaReference, LambdaName][];
+ */
+function buildConfigurationMap(): {[Property in LambdaReference]: LambdaConfiguration} {
   const configurationMap: any = {};
-  for (let [lambdaReference, lambdaName] of entries) {
-    configurationMap[lambdaReference] = new LambdaConfiguration(lambdaName, configurationProps[lambdaName]);
+  for (let reference of Object.keys(configurationProps) as LambdaReference[]) {
+    configurationMap[reference] = new LambdaConfiguration(configurationProps[reference]);
   }
   return configurationMap;
 }
 /** Maps LambdaReference -> Configuration object for that lambda */
 export const lambdaConfigurations = buildConfigurationMap();
 
-
-// Return type of `createLambdas`
-type Lambdas = {[Property in LambdaReference]: Function} 
-
 /** 
  * Creates a Lambda function for every configuration defined in this file and returns it
- * in an object map. 
+ * in an object map. Create `lamdaRole` with `createLambdaRole`.
  */
 export function createLambdas(scope: SecretSantaStack, lambdaRole: Role): Lambdas {
-  // must declare object we are building as type `any`, otherwise
-  // typescript throws a fit because all properties aren't defined at
-  // once
+  // TODO: Add table names as environment variables
+  const environmentVariables = {}
+
+  // must declare object we are building as type `any`, because we can't
+  // define all the properities on the `Lambdas` type all at once
   const lambdas: any = {};
   const layer = new LayerVersion(scope, "SecretSantaLambdaLayer", {
     code: Code.fromAsset('../backend/layer'),
@@ -149,16 +153,17 @@ export function createLambdas(scope: SecretSantaStack, lambdaRole: Role): Lambda
     code: Code.fromAsset('../backend/build'),
     runtime: Runtime.NODEJS_16_X,
     role: lambdaRole,
-    layers: [layer]
+    layers: [layer],
+    environment: environmentVariables
   };
 
-  for (let lambdaReference in Lambda) {
-    const configuration = lambdaConfigurations[lambdaReference as LambdaReference];
+  for (let reference of Object.keys(lambdaConfigurations) as LambdaReference[]) {
+    const configuration = lambdaConfigurations[reference];
 
-    lambdas[lambdaReference] = new Function(scope, configuration.cdkID, {
+    lambdas[reference] = new Function(scope, configuration.cdkID, {
       ...sharedConfiguration,
-      functionName: `secret-santa-${configuration.name}`,
-      handler: `lambda/${configuration.name}.default`, // file should be named same as function name
+      functionName: configuration.name,
+      handler: configuration.handler, // file containing handler should be named same as function name
       description: configuration.description,
       memorySize: configuration.memory,
       timeout: configuration.timeout,
@@ -167,10 +172,20 @@ export function createLambdas(scope: SecretSantaStack, lambdaRole: Role): Lambda
   return lambdas;
 } 
 
-export function createLambdaRole(scope: SecretSantaStack) {
-  return new Role(scope, "LambdaRole", {
+/**
+ * Creates the role used by all Lambdas. Pass the dynamodb tables so that the Lambdas
+ * can be granted read/write access. Create `tables` with `./tables.createTables`
+ */
+export function createLambdaRole(scope: SecretSantaStack, tables: Tables) {
+  const role = new Role(scope, "LambdaRole", {
     assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
     managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")],
     roleName: "SecretSantaLambdaRole"
   })
+
+  for (const table of Object.values(tables)) {
+    table.grantReadWriteData(role)
+  }
+
+  return role
 }
---
+++ cdk/lib/tables.ts
@@ -0,0 +1,111 @@
+import { Attribute, AttributeType, Table } from "aws-cdk-lib/aws-dynamodb"
+import { SecretSantaStack } from "./secret-santa-stack"
+
+export type Tables = {
+  Auth: Table,
+  Users: Table,
+  Players: Table,
+  Games: Table
+}
+
+type TableReference = keyof Tables
+
+type TableConfigurationProps = {
+  partitionKey: string,
+  sortKey?: string
+  globalIndexes?: {
+    name: string,
+    partitionKey: string,
+    sortKey?: string
+  }[]
+}
+
+const tableConfigProps: { [Property in TableReference]: TableConfigurationProps } = {
+  Games: {
+    partitionKey: 'code'
+  },
+  Users: {
+    partitionKey: 'id',
+    globalIndexes: [
+      { name: 'by-phone-number', partitionKey: 'phone-number' },
+      { name: 'by-email', partitionKey: 'email' }
+    ]
+  },
+  Players: {
+    partitionKey: 'game-code',
+    sortKey: 'display-name',
+  },
+  Auth: {
+    partitionKey: 'id',
+    sortKey: 'otp',
+    globalIndexes: [
+      { name: 'by-auth-token', partitionKey: 'auth-token' }
+    ]
+  }
+}
+
+class TableSchema {
+  name: string
+  cdkID: string
+  partitionKey: string
+  sortKey?: string
+  globalIndexes: {
+    name: string
+    partitionKey: string,
+    sortKey?: string
+  }[]
+
+  constructor(name: string, props: TableConfigurationProps) {
+    this.name = `secret-santa-${name.toLowerCase()}-table`
+    this.cdkID = `${name}Table`
+    this.globalIndexes = props.globalIndexes ?? []
+    this.partitionKey = props.partitionKey
+    this.sortKey = props.sortKey
+  }
+}
+
+/*
+ * Maps each defined `TableConfigurationProps` object defined in `tableConfigProps` to a `TableSchema` 
+ * object. Using an actual object instead an object literal lets us fill in default values.
+ */
+function buildConfigurationMap(): { [Property in TableReference]: TableSchema } {
+  const configurationMap: any = {}
+  for (let reference of Object.keys(tableConfigProps) as TableReference[]) {
+    configurationMap[reference] = new TableSchema(reference, tableConfigProps[reference]);
+  }
+  return configurationMap;
+}
+
+export const tableConfigurations = buildConfigurationMap()
+
+
+/** Creates all the tables as defined in the `Tables` type. */
+export function createTables(scope: SecretSantaStack): Tables {
+  const tables: { [ref: string]: Table } = {}
+  for (const tableReference of Object.keys(tableConfigurations) as TableReference[]) {
+    const config = tableConfigurations[tableReference]
+
+    const sortKey: { sortKey?: Attribute } = {}
+    if (config.sortKey) sortKey.sortKey = { name: config.sortKey, type: AttributeType.STRING }
+
+    // create table
+    tables[tableReference] = new Table(scope, config.cdkID, {
+      partitionKey: { name: config.partitionKey, type: AttributeType.STRING },
+      ...sortKey,
+      tableName: config.name,
+    })
+
+    // add indexes
+    for (let globalIndex of config.globalIndexes) {
+      const sortKey: any = {}
+      if (globalIndex.sortKey) sortKey.sortKey = { name: globalIndex.sortKey, type: AttributeType.STRING }
+      tables[tableReference].addGlobalSecondaryIndex({
+        indexName: globalIndex.name,
+        partitionKey: { name: globalIndex.partitionKey, type: AttributeType.STRING },
+        ...sortKey
+      })
+    }
+  }
+
+  return tables as Tables
+}
