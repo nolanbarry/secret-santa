808bce7db3d1a4c6496a16c826b68f403f29b5f2
wurstena <wurstena@byu.edu>
2022-10-01 15:27:23 -0600

    Created and formatted home page

---
+++ frontend/src/components/TitleLogo.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+    
+<template>
+    <div class="title-wrapper">
+        <header class="title">
+            Secret Santa
+        </header>
+    </div>
+</template>
+
+<style>
+    .title {
+        font-family: 'Coiny';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 5rem;
+        color: #FFFFFF;
+        -webkit-text-stroke: 0.27rem #A74141;
+        flex-wrap: wrap;
+    }
+
+    .title-wrapper {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
