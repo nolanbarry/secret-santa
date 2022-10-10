befe142d5ddfb37bc2e0b15efab0a8642de39cf3
wurstena <wurstena@byu.edu>
2022-10-10 16:10:19 -0600

    Fixing merge conflicts and refactoring

--- frontend/src/components/CreateExchange.vue
+++ frontend/src/components/CreateExchange.vue
@@ -27,31 +27,4 @@
         align-items: center;
         justify-content: center;
     }
-
-    .button-div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .button {
-        margin-top: 2rem;
-        display: flex;
-
-        border: none;
-        background-color: #A74141;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        border-radius: 40px;
-        width: 20em;
-        padding:1em;
-
-        text-decoration: none;
-        color: white;
-        font-family: 'Inter' sans-serif;
-        font-size: 1rem;
-        justify-content: center;
-
-        cursor: pointer;
-    }
 </style>
